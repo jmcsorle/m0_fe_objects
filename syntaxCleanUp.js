b939591edd29994647c6f803c50ea7595157c5d6
Jan McSorley <mcsorleyjan@gmail.com>
2023-04-03 18:11:03 -0500

    Correct camelCase in key-value pairs in the syntaxClearnUp.js file.

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -14,7 +14,7 @@ var person = {
 var post1 = {
   imageSrc: "./images/beach.png",
   caption: "At the beach with my besties",
-  timestamp: "4:37 PM August 13, 2019",
+  timeStamp: "4:37 PM August 13, 2019",
   number_likes: 0,
   comments: []
 }
@@ -22,7 +22,7 @@ var post1 = {
 var post2 = {
   imageSrc: "./images/holiday-party.png",
   caption: "What a great holiday party omg",
-  timestamp: "11:37 PM December 31, 2019",
+  timeStamp: "11:37 PM December 31, 2019",
   numberLikes: 13,
   comments: []
 }
